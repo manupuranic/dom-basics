f174c519d6e872236a1e519e8d1d11652192bff2
manupuranic <manupuranic@gmail.com>
2023-06-15 21:00:53 +0530

    assignment 4 tasks done

--- index.js
+++ index.js
@@ -1,11 +1,19 @@
-
 // 1.Get the border to the title
 let headerTitle = document.getElementById("header-title");
 // console.log(headerTitle);
 headerTitle.style.borderBottom = "solid 3px #000";
 
-
 // 2.Make the "Add items" bold and green
 let titles = document.getElementsByClassName("title");
 titles[0].style.fontWeight = "bold";
 titles[0].style.color = "green";
+
+// Assignment 4: GetElementByClassName
+// 1. get the array of elements having class name = "list-group-item"
+let items = document.getElementsByClassName("list-group-item");
+// 2. make the third element of the list to have green background color
+items[2].style.backgroundColor = "green";
+// 3. make all the elements in the list to have bold font
+for (let i = 0; i < items.length; i++) {
+  items[i].style.fontWeight = "bold";
+}
