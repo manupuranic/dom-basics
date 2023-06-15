47b291d945ad08108252af4986716dff03d50239
manupuranic <manupuranic@gmail.com>
2023-06-15 20:40:01 +0530

    Assignment task done

---
+++ index.js
@@ -0,0 +1,11 @@
+
+// 1.Get the border to the title
+let headerTitle = document.getElementById("header-title");
+// console.log(headerTitle);
+headerTitle.style.borderBottom = "solid 3px #000";
+
+
+// 2.Make the "Add items" bold and green
+let titles = document.getElementsByClassName("title");
+titles[0].style.fontWeight = "bold";
+titles[0].style.color = "green";
