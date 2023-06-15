1767ec7d12bab73baa42dddec20525c94822d6f6
manupuranic <manupuranic@gmail.com>
2023-06-15 21:37:57 +0530

    assignment 6 tasks done

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
 // 1.Get the border to the title
-let headerTitle = document.getElementById("header-title");
+// let headerTitle = document.getElementById("header-title");
 // console.log(headerTitle);
-headerTitle.style.borderBottom = "solid 3px #000";
+// headerTitle.style.borderBottom = "solid 3px #000";
 
 // 2.Make the "Add items" bold and green
-let titles = document.getElementsByClassName("title");
-titles[0].style.fontWeight = "bold";
-titles[0].style.color = "green";
+// let titles = document.getElementsByClassName("title");
+// titles[0].style.fontWeight = "bold";
+// titles[0].style.color = "green";
 
 // Assignment 4: GetElementByClassName
 // 1. get the array of elements having class name = "list-group-item"
@@ -20,15 +20,30 @@ titles[0].style.color = "green";
 
 // Assignment 5 getElementByTagName
 // Using getElementByClassName
-let items = document.getElementsByClassName("list-group-item");
-items[2].style.color = "green";
-for (let i = 0; i < items.length; i++) {
-  items[i].style.fontWeight = "bold";
-}
+// let items = document.getElementsByClassName("list-group-item");
+// items[2].style.color = "green";
+// for (let i = 0; i < items.length; i++) {
+//   items[i].style.fontWeight = "bold";
+// }
 
-// Using getElementsByTagName
-let li = document.getElementsByTagName("li");
-li[3].style.color = "orange";
-for (let i = 0; i < li.length; i++) {
-  li[i].style.backgroundColor = "#ccc";
+// // Using getElementsByTagName
+// let li = document.getElementsByTagName("li");
+// li[3].style.color = "orange";
+// for (let i = 0; i < li.length; i++) {
+//   li[i].style.backgroundColor = "#ccc";
+// }
+
+// Assignment 6 querySelector & querySelectorAll
+let secondItem = document.querySelector("li:nth-child(2)");
+secondItem.style.backgroundColor = "Green";
+
+let thirdItem = document.querySelector("li:nth-child(3)");
+thirdItem.style.visibility = "hidden";
+
+let li = document.querySelectorAll("li");
+li[1].style.color = "green";
+
+let odd = document.querySelectorAll("li:nth-child(odd)");
+for (let i = 0; i < odd.length; i++) {
+  odd[i].style.backgroundColor = "green";
 }
